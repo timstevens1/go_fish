e1ad33c1d4f128466071c54d74e8a862d9b4648b
timstevens1 <timstvns3@gmail.com>
2017-02-11 22:03:17 -0500

    wrote make_books function to count decks and remove them from hand

--- deck.py
+++ deck.py
@@ -1,13 +1,14 @@
+from random import shuffle
+
 class Deck:
-
     cards = []
-    def _init_(self):
-        rank = [A,2,3,4,5,6,7,8,9,10,J,Q,K]
-        suit = [C,S,D,H]
-        self.cards = cartesian_product(self,rank,suit)
-        self.cards = random.shuffle(cards)
-
-
+    
+    def __init__(self):
+        rank = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+        suit = ['C','S','D','H']
+        self.cards = self.cartesian_product(rank,suit)
+        shuffle(self.cards)
+        
 
     def cartesian_product(self,list_1,list_2):
         product = []
@@ -16,8 +17,10 @@ class Deck:
                 product.append("{}{}".format(item,thing))
         return product
 
+    
     def go_fish(self):
         return self.cards.pop()
+
 
     def deal_hands(self):
         hand_1 = self.cards[0:7]
--- initialization.py
+++ initialization.py
@@ -1,40 +1,35 @@
+from deck import Deck
+import re
+# input player hand and player counter
+# output player hand with no decks and counter incremented to number of decks
+def make_books(player, counter):
+    # index of rank array is a mapping to num_decks
+    ranks = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+    # counters of number of cards of a given rank in the hand
+    num_decks = [0,0,0,0,0,0,0,0,0,0,0,0,0]
+    for card in player:
+        # increments appropriate rank counter for each card
+        num_decks[ranks.index(card[:-1])] +=1
+    for i in range(12):
+        # 4 cards of a rank in the hand
+        if num_decks[i]>= 4:
+            # increment deck counter
+            counter+=1
+            # remove deck from hand
+            player = [x for x in player if not ranks[i] in x]
+    return player, counter
+
+    
+# initialization statements
 print("Welcome to Go Fish: the computer program! you can play go fish against a computer program here based on 3 different difficulty level.")
 print("Simple: The computer plays randomly.")
 print("Strategic: The computer plays with some strategy.")
 print("Deviant: The computer lies.")
 print("That's about it for now folks. Good luck!")
-
-player_1,player_2 = deck.deal_hands()
-def select_difficulty():
-    difficulty = 1
-
-
-class Deck:
-
-    cards = []
-    def _init_(self):
-        rank = [A,2,3,4,5,6,7,8,9,10,J,Q,K]
-        suit = [C,S,D,H]
-        cards = cartesian_product(self,rank,suit)
-        cards = random.shuffle(cards)
-
-
-
-    def cartesian_product(self,list_1,list_2):
-        product = []
-        for item in list_1:
-            for thing in list_2:
-                product.append("{}{}".format(item,thing))
-        return product
-
-    def go_fish(self):
-        return cards.pop()
-
-    def deal_hands():
-        hand_1 = cards[0:7]
-        hand_2 = cards[7:14]
-        del cards[:14]
-        return hand_1,hand_2
-        
-        
-
+# initialize deck
+deck = Deck()
+#create hands
+player_1, player_2 = deck.deal_hands()
+counter_1, counter_2 = 0,0
+# counter is number of decks
+#the human player goes first human is player_1
